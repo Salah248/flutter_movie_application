a5a6c2ba0a09a99b58c8eb54c89e38d54af31720
Salah248 <salahsadoon248@gmail.com>
2025-02-01 23:21:32 +0200

    First commit Finish Create Splash Screan

---
+++ lib/presentation/sign_up_page/sign_up_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignUpView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/splash_page/splash_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie_application/common/helper/navigation/app_navigation.dart';
+import 'package:flutter_movie_application/presentation/home_page/home_page_view.dart';
+import 'package:flutter_movie_application/presentation/signin_page/sign_in_view.dart';
+import 'package:flutter_movie_application/presentation/splash_page/bloc/cubit/splash_cubit.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+         if(state is UnAuthenticated){
+          AppNavigator.pushReplacement(context, SignInView());
+          
+         }else{
+          AppNavigator.pushReplacement(context, HomePageView());
+
+         }
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/splash-bg.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xff1A1B20).withValues(alpha: 0),
+                    Color(0xff1A1B20),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
